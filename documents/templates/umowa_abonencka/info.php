5269079c2ecfeaae32887b2a616bf8823b5f0f7a
Łukasz Kopiszka <lukasz@alfa-system.pl>
2015-07-20 10:01:48 +0200

    Dokumenty: umowa dodanie logo i wyświetlanej nazwy dokumentu na liście wyboru

--- documents/templates/umowa_abonencka/info.php
+++ documents/templates/umowa_abonencka/info.php
@@ -30,7 +30,7 @@ $engine = array(
 				// you can use other engine
 	'type' => DOC_CONTRACT,			// it's also possible to use array of document types in this field
 	'template' => 'template.html', 		// template file (in 'name' dir)
-	'title' => trans('Umowa abonecka'), 	// description for UI
+	'title' => trans('Internet: Umowa abonecka'), 	// description for UI
 	'content_type' => 'text/html', 		// output file type
 	'output' => 'default.html', 		// output file name
 	'plugin' => 'plugin',			// form plugin (in 'name' dir)
